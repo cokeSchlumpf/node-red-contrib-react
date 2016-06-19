199132ed0fc8d833a001deb94873b7c174ff08ff
Michael Wellner <michael.wellner@de.ibm.com>
2016-06-19 22:10:37 +0200

    Added event binding and tests.

--- src/__tests__/main-test.js
+++ src/__tests__/main-test.js
@@ -2,16 +2,84 @@ jest.dontMock('underscore');
 jest.dontMock('underscore-deep-extend');
 jest.dontMock('lodash');
 jest.dontMock('js-props-validator');
-jest.dontMock('../main.js');
+jest.dontMock('../main');
 
-var ReactRED = require('../main.js');
+var ReactRED = require('../main');
+var Props = ReactRED.Props;
+var Thing = ReactRED.Thing;
+var bindEvent = ReactRED.bindEvent;
 
 describe('ReactRED', function() {
-  it('...', function() {
-    console.log(ReactRED);
-  /*
-  expect(validator.validateValue(13)).toEqual(true);
-  expect(validator.validateValue(9)).toEqual(false);
-  */
+  it('emits events if its state is changed by incoming events', function() {
+    var nodeMessages = [
+      {
+        foo: "bar",
+        payload: {
+          event: "arriveAtHome"
+        }
+      },
+      {
+        foo: "bar",
+        req: {
+          query: {
+            event: "sunset"
+          }
+        }
+      },
+      {
+        foo: "bar"
+      }
+    ];
+
+    var thing = Thing({
+      _stateTypes: {
+        currentTime: bindEvent(
+          Props.number().withDefault(0), [
+            [ "timerEvent", "value", true, (value) => value + 10 ]
+          ]),
+
+        sunIsShining: bindEvent(
+          Props.bool().withDefault(true), [
+            [ "sunrise", true ],
+            [ "sunset", false ]
+          ]),
+
+        somebodyAtHome: bindEvent(
+          Props.bool().withDefault(false), [
+            [ "arriveAtHome", true ],
+            [ "leaveHome", false ]
+          ]),
+
+        lightsWindowOn: Props.bool().withDefault(false),
+
+        lightsKitchenOn: Props.bool().withDefault(false),
+
+        lightsBedroomOn: Props.bool().withDefault(false)
+      },
+
+      _rules: function() {
+        return {
+          lightsWindowOn: !this.sunIsShining && (this.somebodyAtHome || this.currentTime < 2359),
+          lightsKitchenOn: !this.sunIsShining && this.somebodyAtHome && this.currentTime < 2000,
+          lightsBedroomOn: !this.sunIsShining && this.somebodyAtHome
+        };
+      }
+    });
+
+    let result = thing.handle(nodeMessages[0]);
+    expect(result.length).toEqual(0);
+
+    result = thing.handleHttp(nodeMessages[1]);
+    expect(result.length).toEqual(3);
+
+    result = thing.handleEvent(nodeMessages[2], "timerEvent", {
+      value: 1995
+    });
+
+    expect(result.length).toEqual(1);
+    expect(result[0].payload.trigger).toEqual("lightsKitchenOn");
+    expect(result[0].payload.triggerWithValue).toEqual("lightsKitchenOn_false");
+    expect(result[0].payload.oldValue).toEqual(true);
+    expect(result[0].payload.newValue).toEqual(false);
   });
 });
--- src/main.js
+++ src/main.js
@@ -3,23 +3,62 @@ import _Props from 'js-props-validator';
 
 export const Props = _Props;
 
-export const Thing = (thing) => {
-  let _state = thing._stateTypes.valueOrDefault();
-
+export const bindEvent = (property, options) => {
   return {
-    handle: function(msg) {
-      const event = msg.req.query.event;
+    _bind: (thing, stateKey) => {
+      const extend = {};
+
+      _.forEach(options, (option) => {
+        extend[option[0]] = (params) => {
+          const result = {};
+
+          if (option[2]) {
+            result[stateKey] = _.get(params, option[1]);
+          } else {
+            result[stateKey] = option[1];
+          }
+
+          if (_.isFunction(option[3])) {
+            result[stateKey] = option[3](result[stateKey]);
+          }
+
+          return result;
+        };
+      });
+
+      return _.assign({}, thing, extend);
+    },
+
+    property: property
+  };
+}
+
+export const Thing = (thing) => {
+  let _state = {};
+
+  _.forEach(thing._stateTypes, (type, key) => {
+    if (_.isFunction(type._bind)) {
+      thing = type._bind(thing, key);
+      _state[key] = type.property.valueOrDefault();
+    } else {
+      _state[key] = type.valueOrDefault();
+    }
+  });
+
+  const self = _.assign({}, thing, {
+    _handle: (msg, payload) => {
+      const event = payload.event;
       const eventFunc = thing[event];
       let out = [];
 
       if (_.isFunction(eventFunc)) {
-        const stateDiffFromEvent = eventFunc(msg.req.query);
+        const stateDiffFromEvent = eventFunc(payload);
         const newStateBeforeRules = _.assign({}, _state, stateDiffFromEvent);
         const _rules = _.bind(thing._rules, newStateBeforeRules);
         const newStateAfterRules = _.assign({}, _state, _rules(), stateDiffFromEvent);
 
         out = _.reduce(newStateAfterRules, (messages, value, key) => {
-          if (value !== thing._state[key] && !_.has(stateDiffFromEvent)) {
+          if (value !== _state[key] && !_.has(stateDiffFromEvent, key)) {
             messages = _.concat(messages, _.assign({}, msg, {
               payload: {
                 trigger: key,
@@ -37,6 +76,22 @@ export const Thing = (thing) => {
       }
 
       return out;
+    },
+
+    handleEvent: (msg, event, params) => {
+      return self._handle(msg, _.assign({}, params, {
+        event: event
+      }));
+    },
+
+    handleHttp: (msg) => {
+      return self._handle(msg, msg.req.query);
+    },
+
+    handle: (msg) => {
+      return self._handle(msg, msg.payload);
     }
-  };
+  });
+
+  return self;
 };
